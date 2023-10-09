6a747d7315273732eb93cdc8636f76559b8ee170
gbrldrosa <gabrieldrosa21@gmail.com>
2023-10-09 15:24:38 -0300

    Trabalhando com DAO.

--- src/br/com/alura/jdbc/TestaInsercaoEListagemComProduto.java
+++ src/br/com/alura/jdbc/TestaInsercaoEListagemComProduto.java
@@ -1,8 +1,10 @@
-import br.com.alura.jdbc.ConnectionFactory;
+package br.com.alura.jdbc;
+
 import br.com.alura.jdbc.dao.ProdutoDAO;
 import br.com.alura.jdbc.modelo.Produto;
+
 import java.sql.Connection;
-import java.sql.*;
+import java.sql.SQLException;
 import java.util.List;
 
 public class TestaInsercaoEListagemComProduto {
